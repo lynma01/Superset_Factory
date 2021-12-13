c9742acfb47d499df730c1110ce1bdb6b3abd7cb
lynma01 <lynma01@gmail.com>
2021-12-13 17:58:16 -0500

    move app.py to dedicated folder

--- app/models.py
+++ app/models.py
@@ -1,16 +1,13 @@
-#objects.py
-
-class Account(models.Model):
+class Account:
     def __init__(self, username, email, date_joined):
-        self.username = models.ForeignKey(Profile)
+        self.username = username
         self.email = email
         self.date_joined = date_joined
 
     def __str__(self):
         return '%s (%s)' % (self.username, self.email)
 
-
-class Profile(models.Model):
+class Profile:
 
     GENDER_MALE = 'm'
     GENDER_FEMALE = 'f'
