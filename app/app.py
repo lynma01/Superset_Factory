c05a61ce1bba07339621013028c5a8381e2c8d0d
lynma01 <lynma01@gmail.com>
2021-12-16 11:38:57 -0500

    remove space and extra table create statement

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 import factories
 
-
 if __name__ == "__main__":
     print(factories.FemaleProfileFactory())
     print(factories.ProfileFactory())
--- database/create_fixtures.sql
+++ database/create_fixtures.sql
@@ -1,9 +1,1 @@
 CREATE USER postgres;
-CREATE DATABASE docker;
-GRANT ALL PRIVILEGES ON DATABASE docker TO postgres;
-
-CREATE TABLE IF NOT EXISTS accounts (
-    firstname VARCHAR,
-    lastname VARCHAR,
-    username VARCHAR
-);
